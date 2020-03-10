474c4678d64e48bc64d9363d9f86c77c098766af
Anuhya98 <anuhya2641@gmail.com>
2020-03-10 18:06:08 +0530

    day59

---
+++ src/app/models/companies.ts
@@ -0,0 +1,10 @@
+export class Company{
+    id:number;
+    companyname:string;
+    turnover:number;
+    companyceoname:string;
+    boardofdirectors:string;
+    selectstockexchange:string;
+    selectsector:string;
+    aboutcompany:string;
+}
